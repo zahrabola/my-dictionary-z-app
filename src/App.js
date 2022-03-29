456730299d03abe02dd95888dd850f3ef75c9abc
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-03-29 15:16:51 +0100

    update app
    link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
     <div className="App">
       <h1>Hello from Zahra Olanrewaju</h1>
       <SearchDictionary />
+      <footer>
+        <div className="link">
+          <p>Coded by Zahra Olanrewaju</p>
+          <a href="https://github.com/zahrabola/my-w-z-app">
+            Open sourece Github
+          </a>
+          <p>Hosted on Netlify</p>
+        </div>
+      </footer>
     </div>
   );
 }
