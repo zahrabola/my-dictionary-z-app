7b311ecad52a9cb370e09c7877f0764a02ad92ce
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-03-29 17:35:41 +0100

    update . meaningjs

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,13 +5,13 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definition.map(function (definition, index) {
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
               {definition.definition}
               <br />
-              <em> {definition.example} </em>
+              <em>{definition.example}</em>
             </p>
           </div>
         );
