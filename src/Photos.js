da5fcbb214350a1b5f63100ff4c07a521272a9a4
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-04-06 15:33:31 +0100

    update Photos

--- src/Photos.js
+++ src/Photos.js
@@ -3,7 +3,27 @@ import React from "react";
 export default function Photos (props){
     console.log(props.photos);
     if (props.photos){
- return " hello photos";
+ return (<section className="photos"> 
+ <div className="row">
+     {props.photos.map(function (photo, index) {
+         return (
+         
+             <img
+               src={photo.src.landscape}
+               className="img-fluid"
+               alt={photo.src.photographer}
+             />
+       
+         );
+
+     })}
+      </div>
+          
+ </section>);
+
+
+
+
     } else {
         return null;
     }
